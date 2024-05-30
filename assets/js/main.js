547018a19c4f4acdf828c14c64ea5f6a7304422a
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2024-05-30 19:00:16 +0200

    OK

--- assets/js/main.js
+++ assets/js/main.js
@@ -533,5 +533,47 @@ $(document).ready(function () {
             }
         });
     });
+
+
+    function portfolioScroll() {
+        const scroll = document.querySelector(".scroll");
+        let isDown = false;
+        let scrollX;
+        let scrollLeft;
+
+        // Mouse Up Function
+        scroll.addEventListener("mouseup", () => {
+            isDown = false;
+            scroll.classList.remove("active");
+        });
+
+        // Mouse Leave Function
+        scroll.addEventListener("mouseleave", () => {
+            isDown = false;
+            scroll.classList.remove("active");
+        });
+
+        // Mouse Down Function
+        scroll.addEventListener("mousedown", (e) => {
+            e.preventDefault();
+            isDown = true;
+            scroll.classList.add("active");
+            scrollX = e.pageX - scroll.offsetLeft;
+            scrollLeft = scroll.scrollLeft;
+        });
+
+        // Mouse Move Function
+        scroll.addEventListener("mousemove", (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+            let element = e.pageX - scroll.offsetLeft;
+            let scrolling = (element - scrollX) * 2;
+            scroll.scrollLeft = scrollLeft - scrolling;
+        });
+
+    }
+
+    portfolioScroll();
+
 });
 
